0ae1d14987b6a924f4253564f333d3244ac94fb8
Anthony Mangiacapra <tonymang72@gmail.com>
2019-10-06 09:55:20 -0400

    You asked for a merge conflict

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -29,4 +29,30 @@ document.getElementById('submitBtn').onclick = function () {
   xhr.setRequestHeader('Content-Type', 'application/json')
   xhr.send(JSON.stringify(cards))
   document.getElementById('results').style.visibility = 'visible'
+  slide_and_reveal()
+}
+
+//animations and sleep
+function slide_and_reveal(){
+	document.getElementById("search").style.left = "2.5vw";
+	document.getElementById("search").classList.add("animated", "slideInRight");
+	// above line just slides to the right since the card is already visible beforehand
+	document.getElementById("results").style.left = "unset";
+	document.getElementById("results").style.right = "2.5vw";
+	document.getElementById("results").classList.add("animated", "fadeInRight");
+ };
+ async function slide_and_hide(){
+	 // does not currently move positions back to initial
+	document.getElementById("search").classList.remove("animated", "slideInRight");
+	document.getElementById("results").classList.remove("animated", "fadeInRight");
+	document.getElementById("results").classList.add("animated", "fadeOutRight");
+	document.getElementById("search").classList.add("animated", "slideOutRight");
+	// slideOutRight for search here is hacky. It only stays onscreen because its removed later
+	// what I mean is that removing the animation after 1 second (when it finishes) unhides the box
+	await sleep(1000);
+	document.getElementById("results").classList.remove("animated", "fadeOutRight");
+	document.getElementById("search").classList.remove("animated", "slideOutRight");
+ }
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
