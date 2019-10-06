5dfe6cbf0012ab2faecb048d1d5172fb81848c77
Reece Emero <reece.emero98@gmail.com>
2019-10-05 22:28:31 -0400

    Added ajax functionality

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 const express = require('express')
+const logger = require('morgan')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
 
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
+
+app.post('/', function (req, res) {
+  console.log(req.body)
+  res.send('EAT CAKE')
+})
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}...`)
