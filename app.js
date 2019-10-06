ccb02c118118ce9116c487077111674175d0220c
Reece Emero <reece.emero98@gmail.com>
2019-10-06 10:44:13 -0400

    Remove bad header

--- app.js
+++ app.js
@@ -49,7 +49,6 @@ app.post('/', function (req, res) {
     })
     xhr.open('GET', `https://api.tcgplayer.com/v1.9.0/pricing/product/${cardId}`, false)
     xhr.setRequestHeader('Authorization', `Bearer ${config.bearer}`)
-    xhr.setRequestHeader('Connection', 'keep-alive')
     xhr.send(data)
   })
   res.send(JSON.stringify(cardAggregate))
